ec503014926205a5c1e4e03f6e7e9988000dd49e
milagrosr <milagros.r23@gmail.com>
2024-01-25 12:19:27 -0300

    :modificacion vite.config.js

--- proyecto-inmuebles/vite.config.js
+++ proyecto-inmuebles/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/proyecto-inmuebles/',
+  base: 'https://milir23.github.io/proyecto-inmuebles/',
   build: {
     
     assetsDir: "assets",
