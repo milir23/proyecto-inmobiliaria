4cef1061484840baf61539c805820177f2c3f8b3
milagrosr <milagros.r23@gmail.com>
2024-02-04 16:41:12 -0300

    :modificacion vite.config.js

--- proyecto-inmuebles/vite.config.js
+++ proyecto-inmuebles/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://milir23.github.io/proyecto-inmuebles/',
+  base: 'https://milir23.github.io/proyecto-inmuebles',
   
   build: {
     
